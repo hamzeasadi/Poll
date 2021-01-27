a6acd97fc8f487aec70a871f4577b7827dd2413e
Hamzeh Asadi <hamzeasadi1@gmail.com>
2021-01-27 02:02:10 +0000

    create a class to manipulate a database

--- database.py
+++ database.py
@@ -2,37 +2,71 @@ import psycopg2 as pg
 import numpy as np
 
 
-DB_name = 'myDatabase'
-DB_user = 'postgres'
-DB_password = 'hamze95123'
-DB_port = '5432'
-DB_host = 'localhost'
 
-try:
-    conn = pg.connect(database=DB_name, user=DB_user, host=DB_host,
-                      password=DB_password, port=DB_port)
-    print("Database Connected")
+db_info = {'database': 'myDatabase', 'user': 'postgres', 'port': '5432',
+           'host': 'localhost', 'password':'hamze95123'}
 
-except Exception:
-    print("sth went wrong")
+tbl_info = {'filed1': 'ID INT PRIMARY KEY NOT NULL', 'filed2': 'NAME TEXT NOT NULL',
+            'filed3': 'LAST_NAME TEXT NOT NULL', 'filed4': 'EMAIL TEXT',
+            'filed5': 'STUDNET_NUMBER INT NOT NULL'}
 
-cur = conn.cursor()
-# cur.execute("""
-# CREATE TABLE Employee(
-# ID INT PRIMARY KEY NOT NULL,
-# NAME TEXT NOT NULL,
-# EMAIL TEXT NOT NULL
-# )
-# """)
-# conn.commit()
+class Database():
+    """we will create a simple a database manipulation class"""
+    def __init__(self, **db_info):
+        super().__init__()
+        self.db_info = db_info
+        self.con = self.check_connection()
 
-# cur.execute("INSERT INTO Employee (ID, NAME, EMAIL) VALUES(2, 'John', 'John@gmail.com')")
+    def check_connection(self):
+        try:
+            conn = pg.connect(database=self.db_info['database'], host=self.db_info['host'],
+                              port=self.db_info['port'], user=self.db_info['user'],
+                              password=self.db_info['password'])
+        except Exception as e:
+            print(e)
+        else:
+            print("the connection was successfull")
+            return conn
 
-cur.execute('SELECT ID, NAME, EMAIL FROM Employee')
-rows = cur.fetchall()
-for data in rows:
-    print(f'name={data[1]} ID={data[0]} Eemail={data[2]}')
+    def createTable(self, tbl_name, **tbl_info):
+        fields_list = list(tbl_info.values())
+        fields = ", ".join(fields_list)
+        create_table = f"CREATE TABLE {tbl_name}({fields})"
+        # return create_table
+        cur = self.con.cursor()
+        cur.execute(create_table)
+        cur.close()
+        self.con.commit()
+        self.con.close()
 
-# conn.commit()
-conn.close()
+    def deleteTable(self, tbl_name):
+        delete_table = f"DROP TABLE {tbl_name}"
+        cur = self.con.cursor()
+        cur.execute(delete_table)
+        cur.close()
+        self.con.commit()
+        self.con.close()
 
+    def readData(self, tbl_name, *fields):
+        field_list = list(fields)
+        field = ", ".join(fields)
+        command = f"SELECT {field} FROM {tbl_name}"
+        cur = self.con.cursor()
+        cur.execute(command)
+        rows = cur.fetchall()
+        return rows
+
+
+
+dbman = Database(**db_info)
+# dbman.createTable('STUDENT', **tbl_info)
+# dbman.deleteTable('STUDENT')
+
+# read_field = ['ID', 'NAME']
+# rows = dbman.readData('employee', *read_field)
+# for data in rows:
+#     print(f'name={data[1]} ID={data[0]} Eemail={data[2]}')
+
+
+
+# # cur.execute("INSERT INTO Employee (ID, NAME, EMAIL) VALUES(2, 'John', 'John@gmail.com')")
